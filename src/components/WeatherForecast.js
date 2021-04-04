f600654f8e1ae6e09fb886f4b127e6cb41a85960
Mickie4 <40367446+Mickie4@users.noreply.github.com>
2021-04-04 21:56:59 +0100

    created a styling sheet for the temperature element

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   //c6457dceef34bc5b29fe61bd0c8168e1, 0603e85b4ce086e6bb52d7cdc7bcffb5, 17f56a92dfdfe63b71c98d96a2a4942a, 5f472b7acba333cd8a035ea85a0d4d4c
   let lon = props.coordinates.lon;
@@ -18,12 +23,17 @@ export default function WeatherForecast(props) {
   if (loaded && forecast.lat === lat && forecast.lon === lon) {
     return (
       <div className='weatherForecast row'>
-        <WeatherForecastDay data={forecast.daily[1]} />
-        <WeatherForecastDay data={forecast.daily[2]} />
-        <WeatherForecastDay data={forecast.daily[3]} />
-        <WeatherForecastDay data={forecast.daily[4]} />
-        <WeatherForecastDay data={forecast.daily[5]} />
-        <WeatherForecastDay data={forecast.daily[6]} />
+        {forecast.daily.map(function (dailyForecast, index) {
+          if (index < 6) {
+            return (
+              <div className='col forecast' key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
     );
   } else {
--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import "../styles/WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("metric");
-
-  function fahrenheit() {
-    return Math.round((props.celsius * 9) / 5 + 32);
-  }
-
-  function showMetric(event) {
-    event.preventDefault();
-    setUnit("metric");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  if (unit === "metric") {
-    return (
-      <div className='weather-temperature'>
-        <h1 className='current-temp'>{props.celsius}°</h1>
-        <button className='temp-btn-active'>C</button>
-        <button className='temp-btn ' onClick={showFahrenheit}>
-          F
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <div className='weather-temperature'>
-        <h1 className='current-temp'>{fahrenheit()}°</h1>
-        <button className='temp-btn' onClick={showMetric}>
-          C
-        </button>
-        <button className='temp-btn-active'>F</button>
-      </div>
-    );
-  }
+  return (
+    <div className='weather-temperature'>
+      <h1 className='current-temp'>{props.celsius}°</h1>
+      <button className='temp-btn-active'>C</button>
+    </div>
+  );
 }
